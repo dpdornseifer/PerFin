64b577df530b7c3609c5da98f2aa0916ce3ae687
david <david-paul.dornseifer@sap.com>
2016-12-26 14:23:28 +0100

    Fixed a typo

--- examples/playground.py
+++ examples/playground.py
@@ -1,23 +1,28 @@
 import os
 from perfin import importutils as iu
 from perfin import analyticsutils as au
-from perfin import pltutils as plt
 
-# set the environment variable first for the files
-# export PERFIN_FILES=/Users/i841521/PycharmProjects/perfin/tests/files
-#exec(open("/Users/i841521/PycharmProjects/perfin/examples/playground.py
-#    ...: ").read(), globals()
 
+
+# read the environment variable which points to the .csv files
 filepath = os.environ['PERFIN_FILES']
 print('filepath', filepath)
 
+
+# import the .csv files and convert the first column to the datetimeindex
 data = iu.importcsvsfromdirectory(filepath, 1)
 print('raw data', data)
 
+
+# calculate the savings
 savings = au.getsavings(data)
 print('savings', savings)
 
+
+# get the stdev of the savings
 stdev = au.getstdev(savings)
 print('stdev', stdev)
 
+
+# print the available variables
 print('following variables are available now ', ['data', 'savings', 'stdev'])
